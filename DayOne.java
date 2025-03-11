b832b5e6582c74cc870aea9c69432188ada27576
Riley Brotz <rcb2957@rit.edu>
2025-03-11 17:26:04 -0400

    Distance Method Addded

--- DayOne.java
+++ DayOne.java
@@ -1,12 +1,20 @@
 package AdventOfCodeChallenges;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class DayOne{
 
     public List<Integer> sortList(List<Integer> list){
+
+        if(list == null){
+            return list;
+        }
+        if(list.isEmpty()){
+            return list;
+        }
 
         List<Integer> sortedList = new ArrayList<>();
 
@@ -22,17 +30,36 @@ public class DayOne{
 
         int distance = 0;
 
+        if(listOne == null || listTwo == null){
+            return 0;
+        }
+        if(listOne.isEmpty() || listTwo.isEmpty()){
+            return 0;
+        }
+
         sortList(listOne);
         sortList(listTwo);
 
         for(int i = 0; i < listOne.size(); i++){
-            
+            distance += Math.abs(listOne.get(i) - listTwo.get(i));
         }
 
         return distance;
     }
 
-    public static void main(String args){
-
+    public void main(String args){
+        List<Integer> listOne = Arrays.asList(3, 4, 2, 1, 3, 3);
+        List<Integer> listTwo = Arrays.asList(4, 3, 5, 3, 9, 3);
+        int totalDistance = 0;
+        try {
+            totalDistance = distance(listOne, listTwo);
+            System.out.println("The total distance between the lists is " + totalDistance);
+        }
+        catch(NullPointerException e){
+            System.out.println("One of the lists has a null value");
+        }
+        catch (Exception e) {
+           System.out.println("Error Found");
+        }
     }
 }
