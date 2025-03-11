a27b97f2dcac7ccaeda4cb962ff0642499dbe493
Riley Brotz <rcb2957@rit.edu>
2025-03-11 18:17:58 -0400

    Distance Method Addded

--- DayOne.java
+++ DayOne.java
@@ -7,7 +7,9 @@ import java.util.List;
 
 public class DayOne{
 
-    public List<Integer> sortList(List<Integer> list){
+    public static List<Integer> sortList(List<Integer> list){
+
+        List<Integer> unsortedList = new ArrayList<>(list);
 
         if(list == null){
             return list;
@@ -19,14 +21,21 @@ public class DayOne{
         List<Integer> sortedList = new ArrayList<>();
 
         for(int i = 0; i < list.size(); i++){
-            sortedList.add(Collections.min(list));
-            list.remove(Collections.min(list));
+
+            sortedList.add(Collections.min(unsortedList));
+            
+            int minimumIndex = unsortedList.indexOf(Collections.min(unsortedList));
+
+            if(minimumIndex != -1){
+                unsortedList.remove(minimumIndex);
+            }   
+
         }
 
         return sortedList;
     }
 
-    public int distance(List<Integer> listOne, List<Integer> listTwo){
+    public static int distance(List<Integer> listOne, List<Integer> listTwo){
 
         int distance = 0;
 
@@ -37,8 +46,8 @@ public class DayOne{
             return 0;
         }
 
-        sortList(listOne);
-        sortList(listTwo);
+        listOne = sortList(listOne);
+        listTwo = sortList(listTwo);
 
         for(int i = 0; i < listOne.size(); i++){
             distance += Math.abs(listOne.get(i) - listTwo.get(i));
@@ -47,10 +56,13 @@ public class DayOne{
         return distance;
     }
 
-    public void main(String args){
+    public static void main(String[] args){
+
         List<Integer> listOne = Arrays.asList(3, 4, 2, 1, 3, 3);
         List<Integer> listTwo = Arrays.asList(4, 3, 5, 3, 9, 3);
+
         int totalDistance = 0;
+
         try {
             totalDistance = distance(listOne, listTwo);
             System.out.println("The total distance between the lists is " + totalDistance);
