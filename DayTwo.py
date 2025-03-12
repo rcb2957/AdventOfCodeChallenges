17d43941732d9618fbd23604784262c4bfab1d24
Riley Brotz <rcb2957@rit.edu>
2025-03-12 02:20:44 -0400

    Updated Print Statements to Print each array result

--- DayTwo.py
+++ DayTwo.py
@@ -34,12 +34,12 @@ def check_array(array_list):
 def check(main_list):
     for array in main_list:
         if check_array(array):
-            print(array + " is Safe")
+            print(" is Safe")
         else:
-            print(array + " is Unsafe")
+            print(" is Unsafe")
 
 def main():
-    main_list = [[7, 6, 4, 2, 1], [1, 2, 7, 8, 9],[9, 7, 6, 2, 1],[1, 3, 2, 4, 5],[8, 6, 4, 4, 1],[1, 3, 6, 7, 9]]
+    main_list = [[7, 6, 4, 2, 1], [1, 2, 7, 8, 9], [9, 7, 6, 2, 1], [1, 3, 2, 4, 5], [8, 6, 4, 4, 1], [1, 3, 6, 7, 9]]
     check(main_list)
 
 if __name__=="__main__":
