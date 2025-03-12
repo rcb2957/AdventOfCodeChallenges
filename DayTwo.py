45032f15dc146498a73dfaaf761803bb681fa8c1
Riley Brotz <rcb2957@rit.edu>
2025-03-12 02:40:39 -0400

    Added feature to count total safe lists

--- DayTwo.py
+++ DayTwo.py
@@ -37,17 +37,22 @@ def check_array(array_list):
 
 def check(main_list):
 
+    total_safe = 0
+
     for array in main_list:
 
         if check_array(array):
+            total_safe += 1
             print(" is Safe")
         else:
             print(" is Unsafe")
+    return total_safe
 
 def main():
 
     main_list = [[7, 6, 4, 2, 1], [1, 2, 7, 8, 9], [9, 7, 6, 2, 1], [1, 3, 2, 4, 5], [8, 6, 4, 4, 1], [1, 3, 6, 7, 9]]
-    check(main_list)
+    total_safe = check(main_list)
+    print(total_safe + " reports are safe")
 
 if __name__=="__main__":
     main()
