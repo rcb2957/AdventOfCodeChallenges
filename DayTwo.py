bd832ca6630c2eb1a25cab80426aa4b841c00ceb
Riley Brotz <rcb2957@rit.edu>
2025-03-12 01:52:52 -0400

    Set up each array in the list to be checked and have safeness determined

--- DayTwo.py
+++ DayTwo.py
@@ -1,22 +1,23 @@
 def check(main_list):
     for array in main_list:
-        compare = -1
-        for element in array:
-            if compare != -1:
-                difference = compare - element
-                if difference > 3 | difference < -3 | difference == 0:
-                    return False
+        if check_array(array):
+            print(array + " is Safe")
+        else:
+            print(array + " is Unsafe")
+
+def check_array(array_list):
+    compare = -1
+    for element in array_list:
+        if compare != -1:
+            difference = compare - element
+            if difference > 3 | difference < -3 | difference == 0:
+                return False
     return True
-
-
 
 
 def main():
     main_list = [[7, 6, 4, 2, 1], [1, 2, 7, 8, 9],[9, 7, 6, 2, 1],[1, 3, 2, 4, 5],[8, 6, 4, 4, 1],[1, 3, 6, 7, 9]]
-    if check(main_list):
-        print("Safe")
-    else:
-        print("Unsafe")
+    check(main_list)
 
 if __name__=="__main__":
     main()
