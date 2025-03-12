752a0fe038cf2bf7d49b152ef6ec3e8d0dea13c0
Riley Brotz <rcb2957@rit.edu>
2025-03-12 02:37:51 -0400

    Fixed bug that caused false positives from detecting change in increase or decrease direction

--- DayTwo.py
+++ DayTwo.py
@@ -1,17 +1,24 @@
 def update_change(difference):
-    if difference > 0:
-        return 1
     if difference < 0:
+        return 1
+    if difference > 0:
         return -1
 
 def check_change(change, difference):
-    if (change == 1 & difference < 0) or (change == -1 & difference > 0):
+    
+    if (change == 1 and difference > 0) or (change == -1 and difference < 0):
         return False
+    
+    if difference > 3 or difference < -3 or difference == 0:
+        return False
+    
     return True
 
 def check_array(array_list):
+
     change = 0
     compare = -1
+
     for element in array_list:
 
         if compare != -1:
@@ -23,22 +30,22 @@ def check_array(array_list):
             
             if check_change(change, difference) == False:
                 return False
-            
-            if difference > 3 | difference < -3 | difference == 0:
-                return False
         
         compare = element
     
     return True
 
 def check(main_list):
+
     for array in main_list:
+
         if check_array(array):
             print(" is Safe")
         else:
             print(" is Unsafe")
 
 def main():
+
     main_list = [[7, 6, 4, 2, 1], [1, 2, 7, 8, 9], [9, 7, 6, 2, 1], [1, 3, 2, 4, 5], [8, 6, 4, 4, 1], [1, 3, 6, 7, 9]]
     check(main_list)
 
