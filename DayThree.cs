267d658c6bb80b9def5f356d90d73283855f0428
Riley Brotz <rcb2957@rit.edu>
2025-03-20 23:22:54 -0400

    Created Outline methods

---
+++ DayThree.cs
@@ -0,0 +1,14 @@
+public class DayThree{
+
+    public void search(String input){
+        
+    }
+
+    public int multiply(int x, int y){
+        
+    }
+
+    public static void main(String[] args){
+
+    }
+}
