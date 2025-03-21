844d7139fda0d05c92f83625fa5a1d1d0b42269e
Riley Brotz <rcb2957@rit.edu>
2025-03-21 02:46:51 -0400

    Created main method

---
+++ DayFour.js
@@ -0,0 +1,13 @@
+function word_search(array){
+    array.forEach(function(row) {
+        
+    });
+}
+
+function main(){
+    let array = ['MMMSXXMASM', 'MSAMXMSMSA', 'AMXSXMAAMM', 
+                'MSAMASMSMX', 'XMASAMXAMM', 'XXAMMXXAMA', 
+                'SMSMSASXSS', 'SAXAMASAAA', 'MAMMMXMMMM',
+                'MXMXAXMASX'];
+    word_search(array);
+}
