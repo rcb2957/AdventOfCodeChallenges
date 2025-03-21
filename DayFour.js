05c0a9d8eddbd21074a3ffaaa402d135601c0fd8
Riley Brotz <rcb2957@rit.edu>
2025-03-21 03:06:18 -0400

    Added 'X' finding function

--- DayFour.js
+++ DayFour.js
@@ -1,7 +1,20 @@
 function word_search(array){
-    array.forEach(function(row) {
-        
-    });
+    let double_array = []
+    for(let i = 0; i < array.length; i++){
+        double_array[i] = Array.from(array[i]);
+    }
+
+    for(let i = 0; i < double_array.length; i++){
+        for(let j = 0; j < double_array.length; j++){
+            if(double_array[i][j] == 'X'){
+                check_match(double_array, i, j);
+            }
+        }
+    }
+}
+
+function check_match(array, row, cell){
+
 }
 
 function main(){
@@ -11,3 +24,6 @@ function main(){
                 'MXMXAXMASX'];
     word_search(array);
 }
+
+// Add coordinates for row and column
+// Store a list of row and columns that are part of XMAS
