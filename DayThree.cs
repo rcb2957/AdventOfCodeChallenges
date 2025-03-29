2402765b3c79ebbe83bafc61d2f9cba1c491afc4
Riley Brotz <rcb2957@rit.edu>
2025-03-29 14:59:46 -0400

    Added Psuedocode

--- DayThree.cs
+++ DayThree.cs
@@ -1,14 +1,31 @@
 public class DayThree{
 
-    public void search(String input){
-        
+    public void search(string input){
+        // Loop through string until mul is found
+        for(int i = 0; i < input.length(); i++){
+            if(current string = 'mul('){
+                while(i = an integer){
+
+                }
+                if(input == comma){
+
+                }
+                while(i = an integer){
+
+                }
+                if(input == ")"){
+                    
+                }
+            }
+        }
+
     }
 
     public int multiply(int x, int y){
         
     }
 
-    public static void main(String[] args){
-
+    public static void main(string[] args){
+        search("mul(3,4)*&mul[0,9)")
     }
 }
