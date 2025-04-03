8ea6b2ae2d49248220c7a794697687785281bb42
Riley Brotz <rcb2957@rit.edu>
2025-04-03 11:37:29 -0400

    Set up search method to check for corrupted code

--- DayThree.cs
+++ DayThree.cs
@@ -2,30 +2,38 @@ public class DayThree{
 
     public void search(string input){
         // Loop through string until mul is found
+        int mul_count = 0
         for(int i = 0; i < input.length(); i++){
+            string a = "";
+            string b = "";
+            bool corrupted = false;
             if(current string = 'mul('){
                 while(input.charAt(i).IsDigit()){
                     i++;
+                    a += input.charAt(i);
                 }
-                if(input == comma){
-
+                if(input.charAt(i) == ','){
+                    i++;
                 }
-                while(i = an integer){
-
+                while(input.charAt(i).IsDigit()){
+                    i++;
+                    b += input.charAt(i);
                 }
-                if(input == ")"){
-                    
+                if(input.charAt(i) == ')' && !string.IsNullOrEmpty(a) && !string.IsNullOrEmpty(b)){
+                    mul_count++;
+                    multiply(a, b);
                 }
             }
         }
 
     }
 
-    public int multiply(int x, int y){
-        
+    public int multiply(string x, string y){
+        return Int32.Parse(x) * Int32.Parse(y)
     }
 
     public static void main(string[] args){
-        search("mul(3,4)*&mul[0,9)")
+        DayThree day = new DayThree();
+        day.search("mul(3,4)*&mul[0,9)")
     }
 }
