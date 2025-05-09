34b601e544ac449f5fdfe7943cfc49b8e3f57848
Riley Brotz <rcb2957@rit.edu>
2025-05-09 00:51:33 -0400

    Added feature to calculate the row and column of the initial position

--- DaySix.py
+++ DaySix.py
@@ -1,22 +1,23 @@
 def make_grid(grid_string):
-    grid_array = grid_string.splitlines()
-    grid = [len(grid_array)]
+    grid = grid_string.splitlines()
     row_index = 0
 
-    for row in grid_array:
+    for row in grid:
         grid[row_index] = list(row)
         row_index = row_index + 1
 
     return grid
     
-def locate_cursor(grid):
-    grid.index('^')
+def locate_start_position(grid):
+    for grid_row in grid:
+        if '^' in grid_row:
+            return (grid_row.index('^'), grid.index(grid_row))
 
 
 def main():
-    grid_string = "....#.....\n.........#\n..........\n..#.......\n.......#..\n..........\n.#..^.....\n........#.\n#...............#..."
+    grid_string = "....#.....\n.........#\n..........\n..#.......\n.......#..\n..........\n.#..^.....\n........#.\n#.........\n......#..."
     grid = make_grid(grid_string)
-    locate_cursor(grid)
+    print(locate_cursor(grid))
     print(grid)
 
 if __name__=="__main__":
